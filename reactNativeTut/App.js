519254c3aaa8666ee9d5dd9f00dd790927610d2f
hakbar0 <haseeb2000@hotmail.com>
2018-11-30 15:23:19 +0000

    Created an albulm list component

--- reactNativeTut/App.js
+++ reactNativeTut/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Header headerText={'Albums!'} />
+        <AlbumList />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 110,
-    paddingTop: 50,
-    elevation: 10,
-  },
-});
